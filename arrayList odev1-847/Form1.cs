a699701a1e016d1a33226eb647f2b21c6432f856
hayrunnisayilbur <145451185+hayrunnisayilbur@users.noreply.github.com>
2023-12-04 09:37:03 +0300

    Add files via upload

---
+++ arrayList odev1-847/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arrayList_odev1_847
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ArrayList sehirler = new ArrayList();
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            sehirler.Add (txtSehirAdi.Text);
+        }
+    }
+}
